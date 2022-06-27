a58a436d15653a92aafb4cef80f65757560d59dd
mbove77 <mbove77@gmail.com>
2022-06-27 11:43:16 -0300

    .gitignore is reset

--- app/src/main/java/com/bove/martin/pexel/ApiKey.kt
+++
@@ -1,9 +0,0 @@
-package com.bove.martin.pexel
-
-/**
- * Created by Martín Bove on 11-Sep-20.
- * E-mail: mbove77@gmail.com
- */
-object ApiKey {
-    const val PEXELS_TOKEN = "YOUR_API_KEY_HERE"
-}
