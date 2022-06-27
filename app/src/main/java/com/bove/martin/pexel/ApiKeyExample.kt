07c7a903f6b9ce88eac1316cfe25e49fcc75d3f3
mbove77 <mbove77@gmail.com>
2022-06-27 11:50:05 -0300

    Add ApiKeyExample

---
+++ app/src/main/java/com/bove/martin/pexel/ApiKeyExample.kt
@@ -0,0 +1,13 @@
+package com.bove.martin.pexel
+
+/**
+ * Created by Martín Bove on 27/6/2022.
+ * E-mail: mbove77@gmail.com
+ */
+
+// TODO Please rename this class to ApyKey in order to work.
+class ApiKeyExample {
+    object ApiKey {
+        const val PEXELS_TOKEN = "YOUR API KEY HERE"
+    }
+}
