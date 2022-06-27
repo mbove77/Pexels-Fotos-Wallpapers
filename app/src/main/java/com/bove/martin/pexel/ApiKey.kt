d01713f8104a08948c805c9d99344e41fc196272
mbove77 <mbove77@gmail.com>
2022-06-27 12:18:50 -0300

    Rename ApiKey

---
+++ app/src/main/java/com/bove/martin/pexel/ApiKey.kt
@@ -0,0 +1,9 @@
+package com.bove.martin.pexel
+
+/**
+ * Created by Martín Bove on 11-Sep-20.
+ * E-mail: mbove77@gmail.com
+ */
+object ApiKey {
+    const val PEXELS_TOKEN = "YOUR API KEY HERE"
+}
